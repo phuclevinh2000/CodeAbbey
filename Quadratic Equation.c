622f9b6a74d5c88ca1877facb22e28b286ee9b70
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2020-05-22 17:25:39 +0300

    Update Quadratic Equation.c

--- Quadratic Equation.c
+++ Quadratic Equation.c
@@ -2,12 +2,23 @@
 #include <math.h>
 
 int main(void) {
-  int a[100], vao, b[100], c[100];
+  float a[100], b[100], c[100], del, out1[100], out2[100];
+  int vao;
   scanf("%d", &vao);
 
   for(int i=0; i<vao; i++)
   {
-    scanf("%d %d %d", &a[i], &b[i], &c[i]);
+    scanf("%f %f %f", &a[i], &b[i], &c[i]);
+    del=(b[i]*b[i])-4*a[i]*c[i];
+    if(del>=0)
+    {
+      out1[i]=(-b[i] + sqrt(b[i]*b[i] - 4*a[i]*c[i])) / (2*a[i]);
+      out2[i]=(-b[i] - sqrt(b[i]*b[i] - 4*a[i]*c[i])) / (2*a[i]);
+    }
+    if(del<0)
+    {
+      out1[i]=
+    }
   }
 
   return 0;
