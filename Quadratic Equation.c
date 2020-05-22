9ece511eadf5e89a943aaa09311092b19ae7a493
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2020-05-22 17:44:09 +0300

    Update Quadratic Equation.c

--- Quadratic Equation.c
+++ Quadratic Equation.c
@@ -9,16 +9,24 @@ int main(void) {
   for(int i=0; i<vao; i++)
   {
     scanf("%f %f %f", &a[i], &b[i], &c[i]);
+  }
+  for(int i=0; i<vao; i++)
+  {
+    
     del=(b[i]*b[i])-4*a[i]*c[i];
     if(del>=0)
     {
-      out1[i]=(-b[i] + sqrt(b[i]*b[i] - 4*a[i]*c[i])) / (2*a[i]);
+      out1[i]=(-b[i] + sqrt(b[i]*b[i] - 4*a[i]*c[i])) / (2*a[i]);       //cong thuc cua de bai 
       out2[i]=(-b[i] - sqrt(b[i]*b[i] - 4*a[i]*c[i])) / (2*a[i]);
+      printf("%.0f %.0f; ", out1[i], out2[i]);
     }
     if(del<0)
     {
-      out1[i]=
+      del=sqrt(-del);
+      out1[i]=((-b[i])/(2*a[i]));    
+      printf("%.0f+%.0fi %.0f-%.0fi; ", ((-b[i])/(2*a[i])), del/(2*a[i]), ((-b[i])/(2*a[i])), del/(2*a[i]));    //truong hop ra so phuc
     }
+    
   }
 
   return 0;
