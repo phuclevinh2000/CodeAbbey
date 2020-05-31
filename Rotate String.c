7d5cdf9094d8b73aadf0e0f3effb8c554d95ee9e
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2020-05-31 21:57:39 +0300

    Update Rotate String.c

--- Rotate String.c
+++ Rotate String.c
@@ -1,41 +1,30 @@
-#include<stdio.h>         //chua xong
-#include<string.h>
-#include<ctype.h>
-int main()
-{
-  int a, lat[100],j,z,k,h;
-  char name[50][50], namecu[50];
-  scanf("%d", &a);
-  for(int i=0; i<a; i++)
-  {
-    char check[50];
-    scanf("%d", &lat[i]);
-    fgets(name[i], sizeof(name[i]), stdin); // read string
-    //lam truong hop chu duong thi lay tu chu do vo ben trong
-    j=0;
-    do {
-      check[j]=name[i][j];      //check la day truoc
-      j++;
+#include <stdio.h>
+
+void rotate(int n, char *s) {
+    if (n > 0) {
+        printf("%s", s+n);
+        for (int i = 0; i < n; i++)
+            putchar(s[i]);
+        putchar(' ');
     }
-    while(j<=lat[i]);
-    z=j;
-    k=0;
-    do{
-      namecu[k]=name[i][z];        //namecu la day sau
-      k++;
-      z++;
+    else {
+        int c;
+        for (c = 0; s[c] != '\0'; c++)
+            ;
+        printf("%s", s+c+n);
+        for (int i = 0; i < c+n; i++)
+            putchar(s[i]);
+        putchar(' ');
     }
-    while(z<=strlen(name[i]));
-    int o=0, q=0;
-   // puts(namecu);
-   // puts(check);
-    strcat(namecu, check);
-    puts(namecu);
-    
-  }
-  for(int i=0; i<a; i++)
-  {
-    //puts();
-    //printf("%c", name[i][9]);
-  }
+}
+
+main() {
+    int n;
+    scanf("%d", &n);
+    while (n--) {
+        int a;
+        char s[100];
+        scanf("%d %s", &a, s);
+        rotate(a, s);
+    }
 }
