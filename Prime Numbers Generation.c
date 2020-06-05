a66e034e2ac6b4022a65553286870df804b8c4a7
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2020-06-05 17:48:18 +0300

    Create Prime Numbers Generation.c

---
+++ Prime Numbers Generation.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(void) {
+  int a;
+  int p;
+  int i;
+
+  int primes[200000] = {0};
+  int primeIndex = 2;
+  int out[20];
+
+  bool isPrime;
+
+  scanf("%d", &a);
+
+  for(i=0; i<a; i++)
+  {
+    scanf("%d", &out[i]);
+  }
+
+  primes[0] = 2;
+  primes[1] = 3;
+
+    for(p = 5; p <= 2750131; p = p + 2)
+    {
+        isPrime = true;
+
+        for (i = 1; isPrime && p / primes[i] >= primes[i]; ++i)
+            if (p % primes[i] == 0)
+                isPrime = false;
+
+        if (isPrime == true)
+        {
+            primes[primeIndex] = p;
+            ++primeIndex;
+        }
+    }
+  for ( i = 0;  i < a;  ++i )
+    printf ("%i  ", primes[out[i]-1]);
+  return 0;
+}
