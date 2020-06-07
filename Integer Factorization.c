db88120dfdcb15607a4fed16cb37101600bfc625
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2020-06-07 20:32:27 +0300

    Update Integer Factorization.c

--- Integer Factorization.c
+++ Integer Factorization.c
@@ -1,26 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 int main(void) {
-  long long int a, vao[100];
-  scanf("%lld", &a);
+  int a;
+  int p;
+  int i;
 
-  for(int i=0; i<a; i++)
+  int primes[200000] = {0};
+  int primeIndex = 2;
+  long long int out[40];
+
+  bool isPrime;
+  bool last;
+
+  scanf("%d", &a);
+
+  for(i=0; i<a; i++)
   {
-    scanf("%lld", &vao[i]);
+    scanf("%lld", &out[i]);
   }
-  for(int i=0; i<a; i++)
-  {
-    long long int count=2;
-  do{
-    if(vao[i]%count==0)
+
+  primes[0] = 2;
+  primes[1] = 3;
+
+    for(p = 5; p <= 2750131; p = p + 2)       //tao ra day so prime
     {
-      printf("%lld*", count);
-      vao[i]/=count;
+        isPrime = true;
+
+        for (i = 1; isPrime && p / primes[i] >= primes[i]; ++i)
+            if (p % primes[i] == 0)
+                isPrime = false;
+
+        if (isPrime == true)
+        {
+            primes[primeIndex] = p;
+            ++primeIndex;
+        }
     }
-    else count+=2;
-  }
-  while(vao[i]!=count);
-  printf("%lld ", count);
+  
+  for(i=0; i<a; i++)
+  {
+    int j=0;
+    /*last = true;
+    if(out[i]%primes[j]==0)
+      printf("%d*", primes[j]);*/
+    for(j=0; out[i]/primes[j]>=primes[j]; j++)        //check neu nhu chia het thi in so do ra va tiep tuc chia
+    {
+      if(out[i]%primes[j]==0)
+      {
+        printf("%d*", primes[j]);
+        out[i]/=primes[j];
+        j--;
+      }
+      //if(out[i]==primes[j]) 
+    }
+    printf("%lld ", out[i]);
   }
   return 0;
 }
